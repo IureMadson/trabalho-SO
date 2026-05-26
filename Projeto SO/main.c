aa29fab0b27e7602f2d769b15bc1a6c78901d52f
lbragaf <137904745+lbragaf@users.noreply.github.com>
2026-05-26 08:35:45 -0300

    Initialize fiber in main function
    Added fiber support in main function.

--- Projeto SO/main.c
+++ Projeto SO/main.c
@@ -2,14 +2,18 @@
 #include "semaforo.h"
 #include "pausa.h"
 #include "nucleo.h"
+#include <windows.h>
 
 #DEFINE TAM 10 // TAMANHO DO BUFFER
 #DEFINE 20 // PRODUZIR/CONSUMIR
+LPVOID fiberprincipal;
+STR_DESC_PROC atual;
 
 int buffer[TAM];
 int *in, *out;
 
 int main(){
+ fiberprincipal = ConvertThreadToFiber(NULL);
  
  return 0;
 }
