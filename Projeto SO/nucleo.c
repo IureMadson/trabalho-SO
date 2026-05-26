2aad26444931217049e7996bf1b791a0e2044229
lbragaf <137904745+lbragaf@users.noreply.github.com>
2026-05-26 08:26:26 -0300

    Implement process creation and management functions
    Added new functions for process management and updated the initialization of the ready queue.

--- Projeto SO/nucleo.c
+++ Projeto SO/nucleo.c
@@ -17,3 +17,32 @@ void inicia_fila_prontos(void){
     prim = NULL;
     atual = NULL;
 }
+void cria_processo(void (*end_proc)(void), const char *nome_p){
+    DESCRITOR_PROC proc = (DESCRITOR_PROC)malloc(sizeof(DESCRITOR_PROC));
+    strcpy(proc->nome, nome_p);
+    proc->estado = ATIVO;
+    fila_sem(NULL);
+}
+static PTR_DESC_PROC proximo_ativo_depois(PTR_DESC_PROC a_partir){
+    //verifica se a fila circular (prim) existe e não está vazia; 
+    if(/*   */==NULL){
+        printf("Fila não existe ou não está alocada");
+        return;
+    }
+    //define o ponto inicial da busca (usa prim se a_partir for nulo); 
+    if(a_partir==NULL){
+        
+    }
+}
+static void processo_trampolim(void* arg){
+    PTR_DESC_PROC qlqr = (PTR_DESC_PROC)*arg;
+}
+void dispara_sistema(void){
+    
+}
+void yield(void){
+    
+}
+void termina_processo(void){
+    
+}
