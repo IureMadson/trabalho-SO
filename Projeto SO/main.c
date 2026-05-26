eef4c0354e1dee843c832b7f53d8f337b4f601aa
lbragaf <137904745+lbragaf@users.noreply.github.com>
2026-05-26 07:48:28 -0300

    Refactor main.c to remove co-routines and add buffer
    Removed co-routine implementation and added buffer definitions.

--- Projeto SO/main.c
+++ Projeto SO/main.c
@@ -1,46 +1,15 @@
 #include "system.h"
- #include <stdio.h>
- #include <stdlib.h>
- PTR_DESC c01, c02, prin;
- /* co-rotinas (devem aceitar um void *arg) */
- void corotina1(void *arg)
- {
- while(1)
- {
- printf("tic- ");
- transfer(c01, c02); /* Congela c01, pula para c02 */
- }
- }
+#include "semaforo.h"
+#include "pausa.h"
+#include "nucleo.h"
 
- void corotina2(void *arg)
- {
- while(1)
- {
- printf("tac\n");
- transfer(c02, c01); /* Congela c02, pula de volta para c01 */
- }
- }
-/* Programa Principal */
- int main(void)
-{
- system("cls"); /* Alternativa moderna ao clrscr() */
+#DEFINE TAM 10 // TAMANHO DO BUFFER
+#DEFINE 20 // PRODUZIR/CONSUMIR
 
- /* cria descritores de co-rotinas */
- c01 = cria_desc();
- c02 = cria_desc();
- prin = cria_desc();
+int buffer[TAM];
+int *in, *out;
 
- /* associa descritores com as co-rotinas */
- /* Na API do projeto, o argumento do meio é um void* para parâmetros. Passamos NULL.
-*/
- newprocess(corotina1, NULL, c01);
- newprocess(corotina2, NULL, c02);
-
- /* OBRIGATÓRIO: Converte o fluxo principal do Windows em uma Fiber antes de fazer a
-transferência */
- system_init_main(prin);
-
- /* transfere o controle da thread/fiber principal para a co-rotina1 */
- transfer(prin, c01);
-return 0;
+int main(){
+ 
+ return 0;
 }
