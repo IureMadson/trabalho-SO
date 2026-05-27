96062c223c57dcb68469643c77d53a7f40b72f55
lbragaf <137904745+lbragaf@users.noreply.github.com>
2026-05-27 12:27:01 -0300

    Add header guard to semaforo.h

--- Projeto SO/semaforo.h
+++ Projeto SO/semaforo.h
@@ -1,3 +1,6 @@
+#ifndef SEMAFORO_H
+#define SEMAFORO_H
+
 typedef struct{
   int s;
   PTR_DESC_PROC Q;
@@ -6,3 +9,5 @@ typedef struct{
 void inicia_semaforo(semaforo *sem, int n);
 void P(semaforo *sem);
 void V(semaforo *sem);
+
+#endif
