a200708d2ec18902f585b82f39cd7f810ec17cfa
lbragaf <137904745+lbragaf@users.noreply.github.com>
2026-05-29 15:46:43 -0300

    Implement producer and consumer processes
    Added producer and consumer functions for item production and consumption.

--- Projeto SO/main.c
+++ Projeto SO/main.c
@@ -3,22 +3,40 @@
 #include "pausa.h"
 #include "nucleo.h"
 #include <windows.h>
+#include <stdio.h>
+
+void produtor(void);
+void consumidor(void);
 
 int main(){
  int in = 0;
  int out = 0;
+
  inicia_fila_prontos();
- 
- inicia_semaforo(cheio, 0);
- inicia_semaforo(vazio, TAM);
- inicia_semaforo(mutex, 1);
- 
- cria_processo(Produtor, "Produtor");
- cria_processo(Consumidor, "Consumidor");
+
+ cria_processo(produtor, "Produtor");
+ cria_processo(consumidor, "Consumidor");
  printf("Sistema pronto. Iniciando escalonador cooperativo...\n");
  
  dispara_sistema();
  printf("\nSistema finalizado com sucesso.\n");
  
  return 0;
+}
+
+void produtor(void) {
+	int cont = 0;
+	while (cont < 1000) {
+		printf("Produzindo item... %d\n", cont);
+		cont++;
+		yield();
+	}
+}
+void consumidor(void) {
+	int cont = 0;
+	while (cont<1000) {
+		printf("Consumindo item... %d\n", cont);
+		cont++;
+		yield();
+	}
 }
